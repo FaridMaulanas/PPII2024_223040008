14f04641e866233f67b3638049dea7cb9fbe64f0
FaridMaulanas <fmaulana236@gmail.com>
2024-10-06 16:10:10 +0700

    tugas 2

---
+++ Pertemuan1/Tugas/TugasHelloWorldSwing.java
@@ -0,0 +1,28 @@
+package Pertemuan1.Tugas;
+import javax.swing.*;
+
+public class TugasHelloWorldSwing {
+    private static void createAndShowGUI() {
+
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("Hello Farid Maulana Suherman");
+        frame.getContentPane().add(label);
+
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
+    
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+
+}

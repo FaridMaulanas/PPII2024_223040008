647d75bf9676de7ffa37753ac85d1e2136e32025
FaridMaulanas <fmaulana236@gmail.com>
2024-12-30 11:45:09 +0700

    MainFrame

---
+++ pertemuan13/jfc-konkurensi/src/MainFrame.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class MainFrame  {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Contoh konkurensi di swing");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(400, 200);
+            frame.setLayout (new BorderLayout());
+
+            JLabel statusLabel = new JLabel("tekan tombol untuk mulai tugas berat", JLabel.CENTER);
+
+            JButton startButton = new JButton("Mulai");
+            
+            JProgressBar progressBar = new JProgressBar(0, 60);
+            progressBar.setValue(0);
+            progressBar.setStringPainted(true);
+
+            frame.add(statusLabel, BorderLayout.NORTH);
+            frame.add(progressBar, BorderLayout.CENTER);
+            frame.add(startButton, BorderLayout.SOUTH);
+
+            startButton.addActionListener(e -> {
+
+                for (int i = 0; i < 60; i++) {
+                    progressBar.setValue(i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        System.err.println(ex.getMessage());
+                    }
+                    }
+                });
+
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            });
+        }
+}
